a4628f669bcfb260ab881e30b0cafdfeddc2ceb6
Xamanakib <akibzaman6577@gmail.com>
2020-12-21 01:04:59 +0600

    initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let a = screen.width;
+let b = screen.height;
+let c= screen.colorDepth;
+let d = screen.pixelDepth
+
+// console.log(a,b);
+
+document.getElementById("FirstInp").innerHTML = `Width= ${a} px`;
+document.getElementById("SecondInp").innerHTML = `Height= ${b} px`;
+document.getElementById("thirdInp").innerHTML = `${c} `;
+document.getElementById("fourthInp").innerHTML = `${d} `;
+
