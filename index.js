39998d6fa75bb56c6852e131dd98dd0afab465b0
xamanakib <70969527+xamanakib@users.noreply.github.com>
2020-12-31 09:44:01 +0600

    block inspecting

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ document.getElementById("SecondInp").innerHTML = `Height= ${b} px`;
 document.getElementById("thirdInp").innerHTML = `${c} `;
 document.getElementById("fourthInp").innerHTML = `${d} `;
 
+
+
+let sel =document.querySelector('#theFirst');
+sel=document.querySelector('.child');
+console.log(sel);
+document.onkeydown = function(e) {
+  if(event.keyCode == 123) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+     return false;
+  }
+  if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+     return false;
+  }
+}
+
